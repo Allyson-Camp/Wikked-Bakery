71856bd0be9366adcbe74012693f16957a1ebd96
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-09 10:15:01 -0700

    add darkBtn function
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Daisy Orozco <daisyyy.ob@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,14 @@ window.onload = function () {
     const lightBtn = document.querySelector(".lightBtn");
     lightBtn.onclick = function () {
         document.body.style.backgroundColor = "red";
-        document.body.style.color = "green";
+        document.body.style.fontSize = "100px";
     };
+
+    const darkBtn = document.querySelector(".darkBtn");
+    darkBtn.onclick = function () {
+      document.body.style.fontSize = "10px";
+      document.body.style.backgroundColor = "purple";
+    }
     //querySelector can work when selecting an element itself or if you target a class by using the .
     // const lightBtn = document.getElementById("lightBtn");
     // lightBtn.onclick = function () {
