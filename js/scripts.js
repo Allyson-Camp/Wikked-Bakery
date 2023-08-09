462ab65f45da311baf1439e7b0dc8b72e06a29b4
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-09 10:04:55 -0700

    add onload and lightBtn functions
    Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>
    Co-authored-by: Daisy Orozco <daisyyy.ob@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-window.onload = function() {
+window.onload = function () {
+    const h1 = document.querySelector("h1");
+    h1.onmouseover = function () {
+        window.alert("working");
+    };
 
-
+    const lightBtn = document.querySelector(".lightBtn");
+    lightBtn.onclick = function () {
+        document.body.style.backgroundColor = "red";
+        document.body.style.color = "green";
+    };
+    //querySelector can work when selecting an element itself or if you target a class by using the .
+    // const lightBtn = document.getElementById("lightBtn");
+    // lightBtn.onclick = function () {
+    //     document.body.style.backgroundColor = "red";
+    //     document.body.style.color = "black";
+    // };
 };
